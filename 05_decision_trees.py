858254409ae2564729e7b443c3397d5d3063cbb3
Floris Choisy <floris.choisy@gmail.com>
2021-01-11 11:20:11 +0100

    Add original file for assignment 8

---
+++ 05_decision_trees.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+
+colors = ["Yellow", "Yellow", "White", "Brown", "Brown"]
+ball_width = [6, 6, 22, 22, 22]
+sport = ["Tennis", "Tennis", "Football", "Football", "Basketball"]
+
+plt.scatter(colors, ball_width)
+plt.show()
