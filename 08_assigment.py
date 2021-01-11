6164507ba2707a1a1a2b48ba17d6f69be9664615
Floris Choisy <floris.choisy@gmail.com>
2021-01-11 15:42:18 +0100

    Completed assignment + plots

--- 08_assigment.py
+++ 08_assigment.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn import datasets
+import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
@@ -11,6 +12,7 @@ X = iris.data
 y = iris.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
+
 
 def bootstrap(X: np.array, y: np.array, target_size: int) -> tuple:
     """
@@ -28,14 +30,16 @@ def bootstrap(X: np.array, y: np.array, target_size: int) -> tuple:
     """
     assert X.shape[0] == len(y)
 
-    raise NotImplementedError()
+    random_indices = np.random.randint(X.shape[0], size=target_size)
+
+    return X[random_indices], y[random_indices]
 
 
-class RandomForrestClassifier():
+class RandomForestClassifier():
 
     def __init__(self, num_trees: int=100, bootstrapping_size: int=50, feature_subset: int=3):
         """
-        Initializes the RandomForrestClassifier.
+        Initializes the RandomForestClassifier.
 
         Args:
             num_trees (int): Number of weak learner trees.
@@ -43,25 +47,24 @@ class RandomForrestClassifier():
             feature_subset (int): The number of features to use for the
                 splitting criteria.
         """
-        raise NotImplementedError()
-    
+
+        self._forest = [DecisionTreeClassifier(max_features=feature_subset) for _ in range(num_trees) ]
+        self.bootstrapping_size = bootstrapping_size
+
 
     def fit(self, X: np.array, y: np.array):
         """
-        Fitting the random forrest to the data given.
+        Fitting the random forest to the data given.
 
         Args:
             X (ndarray): The data in shape NxM.
             y (ndarray): The target in shape N.
         """
-        assert X.ndim == 2 and y.ndim ==1, "Wrong dimensions for X!"
+        assert X.ndim == 2 and y.ndim == 1, "Wrong dimensions for X!"
         assert X.shape[0] == len(y), "Size of X and y does not match!"
         
-        # TODO: Implement the algorithm to learn a random forrest.
-        # you may use sklearn.tree.DecisionThreeClassifier as the
-        # implementation for the weak learner
-
-        raise NotImplementedError()
+        for tree in self._forest:
+            tree.fit(*bootstrap(X, y, self.bootstrapping_size))
     
     
     def predict(self, X: np.array) -> np.array:
@@ -75,11 +78,46 @@ class RandomForrestClassifier():
             (ndarray): The predictions in shape N
         """
         assert X.ndim == 2
-        raise NotImplementedError()
 
+        all_predictions = []
+        for tree in self._forest:
+            all_predictions.append(tree.predict(X))
+        all_predictions = np.array(all_predictions).T
 
-rf = RandomForrestClassifier()
+        most_common_prediction = []
+        for sample in all_predictions:
+            most_common_prediction.append(np.bincount(sample).argmax())
 
-rf.fit(X_train, y_train)
-y_hat = rf.predict(X_test)
-print(accuracy_score(y_test, y_hat))
+        return most_common_prediction
+
+def print_result():
+    print('Init...', end='')
+    rf = RandomForestClassifier(bootstrapping_size=50)
+    print('OK')
+
+    print('Fit...', end='')
+    rf.fit(X_train, y_train)
+    print('OK')
+    print('Predict...', end='')
+    y_hat = rf.predict(X_test)
+    print(accuracy_score(y_test, y_hat))
+    print('Done')
+
+def plot_boostrapping_sizes():
+    all_bootstrapping_sizes = np.arange(1, 15)
+    # all_bootstrapping_sizes = np.arange(1, 200, step = 20)
+    all_accuracies = []
+    for bootstrapping_size in all_bootstrapping_sizes:
+        rf = RandomForestClassifier(bootstrapping_size=bootstrapping_size)
+        rf.fit(X_train, y_train)
+        y_hat = rf.predict(X_test)
+        all_accuracies.append(accuracy_score(y_test, y_hat))
+
+    import matplotlib.pyplot as plt
+
+    plt.plot(all_bootstrapping_sizes, all_accuracies)
+    plt.xlabel('Bootstrapping size')
+    plt.ylabel('Test accuracy')
+    plt.show()
+
+plot_boostrapping_sizes()
